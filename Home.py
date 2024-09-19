0424771d0c4c0d65b71bfb0d8a81a4aa86766101
Evelyn Tengler <tenglerevelyn@gmail.com>
2024-09-19 12:12:36 -0300

    Tu mensaje de commit

--- Home.py
+++ Home.py
@@ -1,29 +1,27 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-# Customize the sidebar
 markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+Aplicaciones web desarrolladas en la Dirección de Información Geoespacial del IGN
 """
+logo = "LogoIgn.png"
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
+st.title("Agua-Tierra App")
+st.sidebar.title("Aplicaciones web:  DIG - IGN")
+st.sidebar.info(markdown)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Aplicaciones web DIG - IGN")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    Aplicaciones web desarrolladas en el departamento de Ciencia de Datos Geoespaciales de la Dirección de Información Geoespacial del IGN.
     """
 )
 
-st.header("Instructions")
+st.header("Instrucciones")
 
 markdown = """
 1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
