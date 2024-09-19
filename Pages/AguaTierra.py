69eb591ee6a69e9f5ba29eb4a25022d8cac81e13
Evelyn Tengler <58312881+etengler@users.noreply.github.com>
2024-09-19 11:48:36 -0300

    Update AguaTierra.py

--- Pages/AguaTierra.py
+++ Pages/AguaTierra.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
 markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+Aplicaciones web desarrolladas en la Dirección de Información Geoespacial del IGN
 """
+logo = "LogoIgn.png"
 
-st.sidebar.title("Aplicaciones web - DIG - IGN")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
-
 st.title("Agua-Tierra App")
+st.sidebar.title("Aplicaciones web:  DIG - IGN")
+st.sidebar.info(markdown)
+
+
+
+
