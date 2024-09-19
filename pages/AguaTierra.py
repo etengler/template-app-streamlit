090addd43143155605235d7050ed7f223c52edd3
Evelyn Tengler <58312881+etengler@users.noreply.github.com>
2024-09-19 12:26:39 -0300

    Update AguaTierra.py

--- pages/AguaTierra.py
+++ pages/AguaTierra.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import geemap
 
 markdown = """
 Aplicaciones web desarrolladas en la Dirección de Información Geoespacial del IGN
@@ -6,9 +7,11 @@ Aplicaciones web desarrolladas en la Dirección de Información Geoespacial del 
 logo = "LogoIgn.png"
 
 st.sidebar.image(logo)
-st.title("Agua-Tierra App")
 st.sidebar.title("Aplicaciones web:  DIG - IGN")
 st.sidebar.info(markdown)
+
+st.title("Agua-Tierra App")
+st.markdown("Esta aplicación permite...")
 
 user_input = st.text_input("Escriba aqui el nombre del proyecto GEE...")
 
